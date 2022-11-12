f5e3bdb90211cbab6a1c68f4150154f93bf39649
Vlas1d <vlasid753@gmail.com>
2022-11-13 01:31:20 +0200

    test-update

--- script/script.js
+++ script/script.js
@@ -15,7 +15,6 @@ function testWebP(callback) {
     };
     webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
-
 testWebP(function (support) {
 
     if (support == true) {

e31b9431f01cddac5963c1c4cfcdab82d97bfe5e
Vlas1d <vlasid753@gmail.com>
2022-11-12 23:05:50 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,26 @@
+$('.burger-button').on('click', function () {
+    $('.header__menu').toggleClass('menu__burger');
+});
+
+const container = document.querySelector('.main-bg__container');
+document.querySelector('.main-bg__slider').addEventListener('input', (e) => {
+    container.style.setProperty('--position', `${e.target.value}%`);
+});
+
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
